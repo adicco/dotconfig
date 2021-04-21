d60cba0fb139647ab1458e6b6540dede21049835
adicco <adicco@ratanagiri.org.uk>
2021-04-21 16:56:19 +0100

    Some cleanup

--- git-to-local.sh
+++ git-to-local.sh
@@ -3,6 +3,8 @@ for d in */ ; do
 	rsync -aPv "$d" "${HOME}/.config/${d}"
 done
 
+# To do: check if i3lock-color is installed
+
 cp i3-custom-lock "${HOME}/.config/"
 cp i3exit "${HOME}/.config/"
 cp bg*png "${HOME}/.config/"
